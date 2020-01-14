5ea88c71845ffd2564769783ac9223e46e468ecf
Shewa00 <KiraKryt63@mail.ru>
2020-01-14 10:16:20 +0300

    trr

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/macbook/CLionProjects/lab-04
+# Build directory: /Users/macbook/CLionProjects/lab-04/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unit_tests "/Users/macbook/CLionProjects/lab-04/cmake-build-debug/tests")
+set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/macbook/CLionProjects/lab-04/CMakeLists.txt;61;add_test;/Users/macbook/CLionProjects/lab-04/CMakeLists.txt;0;")
